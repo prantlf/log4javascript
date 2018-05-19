b763a13ed27fd3899d3068fd917da262a0338af3
Ferdinand Prantl <prantlf@gmail.com>
2018-05-19 12:47:02 +0200

    Replace earlier sources with the build ouptut of 1.4.13

--- demos/ajax/logger.php
+++
@@ -1,4 +0,0 @@
-<?
-$fp = fopen("postdata.txt", "a");
-fputs($fp, file_get_contents("php://input") . "\r\n");
-?>
--- js/tests/log4javascript_lite.js
+++ js/tests/log4javascript_lite.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Tim Down.
+ * Copyright 2015 Tim Down.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
